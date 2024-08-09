654e312b1f26ce510f03381764d54de647ba15d7
Elbrus <brusbeishe@gmail.com>
2024-08-09 10:49:56 +0600

    FirstLast

---
+++ src/main/java/Codingbat/warmup1/String1/array1/Firstlast.java
@@ -0,0 +1,7 @@
+package Codingbat.warmup1.String1.array1;
+
+public class Firstlast {
+    public boolean firstLast6(int[] nums) {
+        return (nums [0] == 6 || nums [nums.length-1] == 6);
+    }
+}
