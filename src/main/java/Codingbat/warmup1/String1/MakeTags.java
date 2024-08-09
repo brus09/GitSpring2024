3a1e397d4eac48b1757e93faa9bf828ed6f7ad3c
Elbrus <brusbeishe@gmail.com>
2024-08-09 10:13:05 +0600

    String1/MakeTags

---
+++ src/main/java/Codingbat/warmup1/String1/MakeTags.java
@@ -0,0 +1,8 @@
+package Codingbat.warmup1.String1;
+
+public class MakeTags {
+    public String makeTags(String tag, String word) {
+        return "<" + tag + ">" +  word +  "<" + "/" + tag + ">";
+    }
+
+}
